1b66c811bcb270bc86ccf662d2db8543b888e5ba
singhuacai <singhuacai@gmail.com>
2021-11-16 23:56:34 +0800

    rewrite the ticket prices in the campaign page

--- public/JS/campaign.js
+++ public/JS/campaign.js
@@ -1,5 +1,14 @@
 const concert_id = window.location.search.split("?id=")[1];
 let Authorization = localStorage.getItem("Authorization");
+const currency = "NT$";
+
+const rewiteTicketPrices = (ticketPricesArray) => {
+  return ticketPricesArray
+    .map((money) => {
+      return currency + money;
+    })
+    .join("、");
+};
 
 $.ajax({
   url: `/api/1.0/concerts/details?id=${concert_id}`,
@@ -20,12 +29,16 @@ $.ajax({
       );
       $("#notice").html(`<p>${res.data.notice}</p>`);
       for (let i = 0; i < res.data.concert_info.length; i++) {
+        const ticket_prices = rewiteTicketPrices(
+          res.data.concert_info[i].ticket_prices
+        );
+
         $("#date_location_price").append(
           `
         <tr>
           <td>${res.data.concert_info[i].concert_datetime}</td>
           <td style="word-wrap:break-word;">${res.data.concert_location}</td>
-          <td>${res.data.concert_info[i].ticket_prices}</td>
+          <td>${ticket_prices}</td>
           <td><button class = "booking-button" onclick = "javascript:location.href='/order.html?concertId=${concert_id}&concertDateId=${res.data.concert_info[i].concert_date_id}'">立即訂購</button></td>
         </tr>
         `
